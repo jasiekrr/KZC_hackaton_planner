fe4ce67e98072bcb459abb57671bcef59d5e0cbb
uxert <uxert@wp.pl>
2024-05-25 23:48:06 +0200

    Nasza najlepsza (bo jedyna) baza danuch

--- ReadmeBranchBackend.md
+++ ReadmeBranchBackend.md
@@ -1,4 +1,11 @@
 Ta gałąź dotyczy backendu
 
 Work done as for now:
--absolutnie nic
+zrobione są wszystkie kontrakty *(na chwilę obecną wszystkie)* do komunikacji z frontendem.
+Przetestowane, działa we front endzie tak, jak powinno. Kontrakt na chwilę obecną zostaje oficjalnie uznazny za zamknięty.
+
+Teraz zadanie - niezwykle trudne, I know, to nasza najlepsza **(bo jedyna xD)** "baza danych"
+tj. absolutny szczyt technologii jakim jest zapisywanie wszystkiego w olbrzymich dwóch jsonach.
+
+Zajęło to zdecydowanie więcej czasu niż powinno, ale ***działa!!!!!!***
+Profesjonalna baza danych jest
--- src/main.py
+++ src/main.py
@@ -1,11 +1,99 @@
 from fastapi import FastAPI, HTTPException, Body
 from pydantic import BaseModel
 from typing import List, Annotated
-
-app = FastAPI()
+import json
+import os
 
 ID_counter: int = 0
 
+actFileName = "activities.txt"
+subjFileName = "subjects.txt"
+is_data_up_to_date = False  # when program begins data is not read from the files - it needs to read it in order to work
+
+subjects = {
+    "1": {"subjectName": "TGiS",
+     "mainTeacher": "Zbigniew Tarapata",
+     "ects": 4,
+     "formats": [
+         {
+             "formatType": "lecture",
+             "numberOfHours": 30
+         },
+         {
+             "formatType": "laboratory",
+             "numberOfHours": 40
+         }
+     ]
+     }
+}
+
+activities: dict = {}
+
+class Activity:
+    studentId: int
+    subjectName: str
+    mainTeacher: str
+    format: str
+    type: str
+    deadline: str
+    done: str
+    Id: int
+
+
+
+def read_data():
+    global ID_counter
+    global subjects
+    global activities
+    global is_data_up_to_date
+    if os.path.exists(actFileName):
+        with open(actFileName, "r") as f:
+            all_activities = f.readlines()
+            for act in all_activities:
+                jsondict = eval(act)
+                for key, value in jsondict.items():
+                    if int(key) > ID_counter:
+                        ID_counter = int(key)
+                    activity = Activity()
+                    activity.__dict__ = eval(jsondict[key]).copy()
+                    activities[int(key)] = activity
+
+
+
+    if  os.path.exists(subjFileName):
+        with open(subjFileName, "r") as f:
+            all_subjects = f.readlines()
+            for sub in all_subjects:
+                pass
+
+
+
+if is_data_up_to_date is False:
+    read_data()
+    print("\n----------Data was read from the database, app is ready for work---------\n")
+    is_data_up_to_date = True
+
+
+app = FastAPI()
+
+
+def write_data(what_to_write):
+    """
+
+    :param what_to_write: if it is "sub" it writes subjects to the "database". If it is "act" it does so for
+    the activities. Every other value is ignored
+    :return:
+    """
+    if what_to_write == "sub":
+        with open(subjFileName, "w") as f:
+            for key, val in subjects.items():
+                f.write( str({str(key) : str(val.__dict__)}) + "\n")
+    elif what_to_write == "act":
+        with open(actFileName, "w") as f:
+            for key, val in activities.items():
+                print(key, val.__dict__)
+
+                f.write(str({str(key): str(val.__dict__)}) + "\n")
 
 class Subject(BaseModel):
     subjectName: str
@@ -24,7 +112,7 @@ class ActivityRequest(BaseModel):
 
 
 class ChangeActivityRequest(BaseModel):
-    ID: int
+    Id: int
     studentId: int
     subjectName: str
     mainTeacher: str
@@ -34,35 +122,10 @@ class ChangeActivityRequest(BaseModel):
     done: str
 
 
-class Activity:
-    studentId: int
-    subjectName: str
-    mainTeacher: str
-    format: str
-    type: str
-    deadline: str
-    done: str
-    ID: int
 
 
-subjects = [
-    {"subjectName": "TGiS",
-     "mainTeacher": "Zbigniew Tarapata",
-     "ects": 4,
-     "formats": [
-         {
-             "formatType": "lecture",
-             "numberOfHours": 30
-         },
-         {
-             "formatType": "laboratory",
-             "numberOfHours": 40
-         }
-     ]
-     }
-]
 
-activities: dict = {}
+
 
 
 @app.post("/activities/")
@@ -75,8 +138,9 @@ def create_activity(activity: Annotated[ActivityRequest, Body()]):
     newActivity.__dict__ = activity.__dict__.copy()
 
     newActivity.done = "false"
-    newActivity.ID = tempID
+    newActivity.Id = tempID
     activities[tempID] = newActivity
+    write_data("act")
     return newActivity
 
 
@@ -87,18 +151,19 @@ def read_activities():
 
 @app.get("/activities/{index}")
 def read_activities(index: int):
+    print(activities)
     if index not in activities:
         return 404
     return activities[index]
 
 
 @app.put("/activities/")
-async def create_activity(activity: Annotated[ChangeActivityRequest, Body()]):
-    subj = activity.subjectName
-    changedID = activity.ID
+async def change_activity(activity: Annotated[ChangeActivityRequest, Body()]):
+    changedID = activity.Id
     if changedID in activities:
         oldActivity = activities[changedID]
         oldActivity.__dict__ = activity.__dict__.copy()
+        write_data("act")
         return activity
 
 @app.delete("/activities/{index}", status_code=200)
@@ -106,6 +171,7 @@ def remove_act(index: int):
     if index not in activities:
         return 404
     del activities[index]
+    write_data("act")
 
 
 
@@ -156,6 +222,6 @@ async def delete_subject(id: int):
 
 
 if __name__ == "__main__":
-    import uvicorn
 
+    import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
